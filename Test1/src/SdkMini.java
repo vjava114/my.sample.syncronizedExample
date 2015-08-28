5b3283eccf7692516a0ea0b68e50f6a977493429
vjava114 <vjava114@gmail.com>
2015-08-28 10:24:34 +0900

    11

--- Test1/src/SdkMini.java
+++ Test1/src/SdkMini.java
@@ -1,3 +1,14 @@
+/**
+ * 
+ * @author kang
+ * 
+ * 
+ * - 로직 설명 - 
+ * 1. gps, ble, network 처리결과를 1개씩 수행한다
+ * 2. 처리 결과를 서버로 전송한다 // sendServer();
+ * 3. 서버 전송까지 성공 했다면 모든 로직을 중단한다.
+ *
+ */
 public class SdkMini {
 
 	public static boolean ISSEND = false;
